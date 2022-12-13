769a8fcb7a41cba9c3b38920414b2055ac6b58bd
Nirbhay kumar <83468600+Nirbhay97@users.noreply.github.com>
2022-12-13 17:48:59 +0530

    Add files via upload

---
+++ src/notes.js
@@ -0,0 +1,29 @@
+const notes = [
+
+    {
+        key: 1,
+        title: "username",
+        content: "hi there, this is the first content section"
+    },
+
+    {
+        key: 2,
+        title: "username",
+        content: "hi there, this is the first content section"
+    },
+
+    {
+        key: 3,
+        title: "username",
+        content: "hi there, this is the first content section"
+    },
+
+    {
+        key: 4,
+        title: "username",
+        content: "hi there, this is the first content section"
+    }
+   
+]
+
+export default notes;
